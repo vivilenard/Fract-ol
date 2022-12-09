b89b5073c27c2c3d830b44d64c33df9e4a25c371
Vivien Lenard <vlenard@1-B-5.42heilbronn.de>
2022-12-09 19:56:02 +0100

    pixel

--- play.c
+++ play.c
@@ -1,17 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdio.h>
+#include <string.h>
 #include "MLX42/MLX42.h"
-#define WIDTH 720
-#define HEIGHT 480
+#define WIDTH 1000
+#define HEIGHT 1000
+#define BPP sizeof(int32_t)
 
-static void ft_hook(void* param)
+/*static void ft_hook(void* param)
 {
 	const mlx_t* mlx;
 
 	mlx = param;
 	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
 }
+*/
 
 int main ()
 {
@@ -20,11 +22,13 @@ int main ()
 	mlx = mlx_init(WIDTH, HEIGHT, "play", true);
 	if (!mlx)
 		return(EXIT_FAILURE);
-	img = mlx_new_image(mlx, 200, 200);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		return (EXIT_FAILURE);
-	mlx_put_pixel(img, 0, 0, 0x0000FF);
-	mlx_loop_hook(mlx, ft_hook, mlx);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	//if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+	//	return (EXIT_FAILURE);
+	mlx_image_to_window(mlx, img, 5, 5);
+	memset(img->pixels, 75, img->width * img->height * BPP);
+	mlx_put_pixel(img, 500, 500, 0xFFFFFFFF);
+	//mlx_loop_hook(mlx, ft_hook, mlx); */
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
